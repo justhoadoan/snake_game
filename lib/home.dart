7f3d26cdb10afecd3f518e3369fd4e6753afde18
justhoadoan <hoatrongdoan1710@gmail.com>
2023-11-09 20:34:47 +0700

    First step to build app

---
+++ lib/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:snakegame/blank_pixel.dart';
+ 
+ 
+ class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super (key : key );
+  
+  @override
+  State<HomePage> createState() => _HomePageState();
+ }
+ class _HomePageState extends State<HomePage>{
+//grid dimensions
+int rowSize = 10;
+int totalNumberOfSquares = 100;
+// snake postition
+List<int> snakePos = [
+0,
+1,
+2,
+];
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          //scores
+          Flexible(
+
+      fit: FlexFit.tight,
+      child: Container(
+            
+),
+          ),
+          //game grid
+           Flexible(
+            fit: FlexFit.tight,
+            flex: 3,
+      child: GridView.builder(
+        itemCount: totalNumberOfSquares,
+        physics: const  NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: rowSize ), 
+      itemBuilder: (context, index){
+        return const BlankPixel();
+      }),
+        ),
+                
+          
+          
+          
+          //play button
+           Flexible(
+
+      fit: FlexFit.tight,
+      child: Container(
+            
+),
+          ),
+        ]
+          ),
+          );
+  }
+      
+
+
+  }
+  
+  
